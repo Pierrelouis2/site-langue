de911f2363981d1748447e73ace2a53e594b3ee2
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2022-10-24 22:23:25 +0200

    ajout salles

--- Filtre.js
+++ Filtre.js
@@ -10,12 +10,16 @@ async function recup_data(dir) {
 async function filtre(){
 
     let nom = String(document.getElementById("nom").value) //on recup le nom par l'id
-    let lst_donnee = await recup_data("/json/lst_personne.json") //on recup la liste de data
-    console.log("test");
-
+    let lst_donnee = await recup_data("/json/lst_personne.json") //on recup la liste de data des personnes 
+    let lst_salles = await recup_data("/json/lst_salles.json") //on recup la liste de data des salles
     for(id_nom in lst_donnee){
-        if(lst_donnee[id_nom].nom == nom){
-            console.log(lst_donnee[id_nom]);
+        if(lst_donnee[id_nom].NOM == nom){
+            for(tag in lst_salles){
+                if(lst_salles[tag].TAG == lst_donnee[id_nom].LV1){  //on compare le tag de la salle avec le tag de la personne
+                    console.log(lst_salles[tag].SALLE)
+                }
+            }
+            console.log("test2");
         }
     }
 
