228ea79db842db93e37332d0b57952618e5463d8
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2022-10-25 22:25:35 +0200

    test sort

---
+++ sort.py
@@ -0,0 +1,21 @@
+file1 = open("file1.txt", "r")
+file2 = open("data_sort.txt",'w')
+
+lst = file1.readlines()
+valren =[]
+cpt = 0
+lst =[]
+for i in lst:
+    
+    lst.append(i)
+    cpt += 1 
+       
+    
+    if cpt == 10:
+        #file2.write(valren)
+        valren.append(lst)
+        print(valren)
+        lst = []
+        cpt = 0
+          
+        print('test')
